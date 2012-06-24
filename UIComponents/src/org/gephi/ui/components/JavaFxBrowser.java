8655609ef9c1f17edc6a3481b929518d32434fb4
Vikash Anand <vikashanand.mit@gmail.com>
2012-06-25 04:36:03 +0530

    files to be deleted

--- UIComponents/src/org/gephi/ui/components/JavaFxBrowser.java
+++ UIComponents/src/org/gephi/ui/components/JavaFxBrowser.java
@@ -1,41 +1,41 @@
 /*
-Copyright 2008-2012 Gephi
-Authors : Vikash Anand <vikash.anand@gephi.org>
-Website : http://www.gephi.org
+ Copyright 2008-2012 Gephi
+ Authors : Vikash Anand <vikash.anand@gephi.org>
+ Website : http://www.gephi.org
 
-This file is part of Gephi.
+ This file is part of Gephi.
 
-DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 
-Copyright 2012 Gephi Consortium. All rights reserved.
+ Copyright 2012 Gephi Consortium. All rights reserved.
 
-The contents of this file are subject to the terms of either the GNU
-General Public License Version 3 only ("GPL") or the Common
-Development and Distribution License("CDDL") (collectively, the
-"License"). You may not use this file except in compliance with the
-License. You can obtain a copy of the License at
-http://gephi.org/about/legal/license-notice/
-or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
-specific language governing permissions and limitations under the
-License.  When distributing the software, include this License Header
-Notice in each file and include the License files at
-/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
-License Header, with the fields enclosed by brackets [] replaced by
-your own identifying information:
-"Portions Copyrighted [year] [name of copyright owner]"
+ The contents of this file are subject to the terms of either the GNU
+ General Public License Version 3 only ("GPL") or the Common
+ Development and Distribution License("CDDL") (collectively, the
+ "License"). You may not use this file except in compliance with the
+ License. You can obtain a copy of the License at
+ http://gephi.org/about/legal/license-notice/
+ or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+ specific language governing permissions and limitations under the
+ License.  When distributing the software, include this License Header
+ Notice in each file and include the License files at
+ /cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+ License Header, with the fields enclosed by brackets [] replaced by
+ your own identifying information:
+ "Portions Copyrighted [year] [name of copyright owner]"
 
-If you wish your version of this file to be governed by only the CDDL
-or only the GPL Version 3, indicate your decision by adding
-"[Contributor] elects to include this software in this distribution
-under the [CDDL or GPL Version 3] license." If you do not indicate a
-single choice of license, a recipient has the option to distribute
-your version of this file under either the CDDL, the GPL Version 3 or
-to extend the choice of license to its licensees as provided above.
-However, if you add GPL Version 3 code and therefore, elected the GPL
-Version 3 license, then the option applies only if the new code is
-made subject to such option by the copyright holder.
+ If you wish your version of this file to be governed by only the CDDL
+ or only the GPL Version 3, indicate your decision by adding
+ "[Contributor] elects to include this software in this distribution
+ under the [CDDL or GPL Version 3] license." If you do not indicate a
+ single choice of license, a recipient has the option to distribute
+ your version of this file under either the CDDL, the GPL Version 3 or
+ to extend the choice of license to its licensees as provided above.
+ However, if you add GPL Version 3 code and therefore, elected the GPL
+ Version 3 license, then the option applies only if the new code is
+ made subject to such option by the copyright holder.
 
-Contributor(s):
+ Contributor(s):
 
  Portions Copyrighted 2011 Gephi Consortium.
  */
@@ -57,49 +57,50 @@ import javafx.scene.web.WebView;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-
 /**
  *
  * @author Vikash Anand
  */
 public class JavaFxBrowser {
+
     private static void initAndShowGUI() {
         JFrame frame = new JFrame("HTML Report");
         final JFXPanel fxPanel = new JFXPanel();
         frame.add(fxPanel);
-        Dimension dimension = new Dimension(700,600);
+        Dimension dimension = new Dimension(700, 600);
         frame.setSize(dimension);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         Platform.runLater(new Runnable() {
+
             @Override
             public void run() {
-            initFX(fxPanel);
+                initFX(fxPanel);
             }
-       });
+        });
     }
-    
+
     private static void initFX(JFXPanel fxPanel) {
         Scene scene = createScene();
         fxPanel.setScene(scene);
     }
-    
+
     private static Scene createScene() {
-        Scene scene = new Scene(new Browser(),750,500, Color.web("#666970"));
-        scene.getStylesheets().add("webviewsample/BrowserToolbar.css");        
+        Scene scene = new Scene(new Browser(), 750, 500, Color.web("#666970"));
+        scene.getStylesheets().add("webviewsample/BrowserToolbar.css");
         return (scene);
     }
-    
+
     public JavaFxBrowser() {
         initAndShowGUI();
     }
 }
 
 class Browser extends Region {
- 
+
     final WebView browser = new WebView();
     final WebEngine webEngine = browser.getEngine();
-     
+
     public Browser() {
         //apply the styles
         getStyleClass().add("browser");
@@ -107,25 +108,29 @@ class Browser extends Region {
         webEngine.load("http://www.google.com");
         //add the web view to the scene
         getChildren().add(browser);
- 
+
     }
+
     private Node createSpacer() {
         Region spacer = new Region();
         HBox.setHgrow(spacer, Priority.ALWAYS);
         return spacer;
     }
- 
-    @Override protected void layoutChildren() {
+
+    @Override
+    protected void layoutChildren() {
         double w = getWidth();
         double h = getHeight();
-        layoutInArea(browser,0,0,w,h,0, HPos.CENTER, VPos.CENTER);
+        layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
     }
- 
-    @Override protected double computePrefWidth(double height) {
+
+    @Override
+    protected double computePrefWidth(double height) {
         return 750;
     }
- 
-    @Override protected double computePrefHeight(double width) {
+
+    @Override
+    protected double computePrefHeight(double width) {
         return 500;
     }
 }
